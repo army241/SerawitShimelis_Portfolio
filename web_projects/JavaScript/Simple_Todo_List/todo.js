a4a2246bb035ad40c1c78d54830fddf80147d0b1
Army241 <serawitshimelis1998@gmail.com>
2026-02-24 21:39:00 +0300

    Modify my project

--- web_projects/JavaScript/Simple_Todo_List/todo.js
+++ web_projects/JavaScript/Simple_Todo_List/todo.js
@@ -1,17 +1,31 @@
 const todoList=[
-    
+    {
+        name: 'Go to the gym',
+        dueDate: '2026-02-22',
+    },
+    {
+        name: 'Watch Youtube',
+        dueDate: '2026-02-23'
+    }
 ];
+
 function renderTodo(){
     let inputHtml ='';
     for (let i=0;i<todoList.length;i++){
-        const todo=todoList[i];
-        const html=`<p>- ${todo}  
+        const todoObject=todoList[i];
+        //const name=todoObject.name;
+        //const dueDate=todoObject.dueDate;
+        const {name,dueDate}=todoObject;
+        const html=`
+        <div>${name}</div>      
+        <div>${dueDate}</div>
         <button onclick="
         todoList.splice(${i},1);
         renderTodo();
         "
+        class="delete-button"
         >Delete</button>
-        </p>`;
+        `;
         inputHtml+=html;
     }
     
@@ -21,9 +35,14 @@ function renderTodo(){
 
 function addTodo(){
     let inputElement=document.querySelector('.js-todo-name');
+    let dateInput=document.querySelector('.js-todo-date');
+    let dueDate = dateInput.value;
     let name=inputElement.value;
-    todoList.push(name);
-    console.log(todoList);
+    todoList.push({
+        // name: name, dueDate: dueDate
+        name,dueDate});
+    
     inputElement.value='';
     renderTodo();
 }
+
